1ee41d0221511013d08f0947b0033eb278d709e4
Nazar Zhuhan <nazarzhuhan@gmail.com>
2024-09-03 13:21:06 +0300

    Initial commit

---
+++ services/discovery-service/src/test/java/org/development/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.development.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
